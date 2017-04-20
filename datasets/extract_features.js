535e76f989ac9b511be75497076de5f0fa625d90
Bhargav Kowshik <bhargav@mapbox.com>
2017-04-20 13:45:19 +0530

    Add labels from osmcha dump as features

--- datasets/extract_features.js
+++ datasets/extract_features.js
@@ -8,29 +8,42 @@ const extractFeatures = require(path.join(__dirname, '../gabbar/helpers/real_cha
 const formatFeatures = require(path.join(__dirname, '../gabbar/helpers/real_changesets')).formatFeatures;
 const csv = require('csv');
 
-if (!argv.changesets) {
+if (!argv.changesets || !argv.realChangesets) {
     console.log('');
     console.log('Usage: node extract_features.js OPTIONS');
     console.log('');
     console.log('  OPTIONS');
-    console.log('    --changesets   changesets.json     Line delimited file with real changesets.');
+    console.log('    --changesets       changesets.csv          Changesets dump from osmcha.');
+    console.log('    --realChangesets   realChangesets.json     Line delimited file with real changesets.');
     console.log('');
     process.exit(0);
 }
 
-const reader = readline.createInterface({
-    input: fs.createReadStream(argv.changesets),
-    output: null
-});
+csv.parse(fs.readFileSync(argv.changesets), (error, rows) => {
+    let harmful = {}
+    for (let row of rows) {
+        // Format: 0th column is changeset ID and 15th column has harmful detail.
+        harmful[row[0]] = row[15];
+    }
 
-reader.on('line', line => {
-    let realChangeset = JSON.parse(line);
+    const reader = readline.createInterface({
+        input: fs.createReadStream(argv.realChangesets),
+        output: null
+    });
 
-    extractFeatures(realChangeset)
-    .then(features => {
-        features = formatFeatures(features);
-        csv.stringify([features], function (error, result) {
-            if (!error) process.stdout.write(result);
-        })
+    reader.on('line', line => {
+        let realChangeset = JSON.parse(line);
+
+        extractFeatures(realChangeset)
+        .then(features => {
+            features = formatFeatures(features);
+
+            let changesetID = features[0];
+            features.splice(1, 0, harmful[changesetID]);
+
+            csv.stringify([features], function (error, result) {
+                if (!error) process.stdout.write(result);
+            })
+        });
     });
 });
