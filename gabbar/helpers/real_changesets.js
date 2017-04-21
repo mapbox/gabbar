d6077d17ba5a0bbdabd20d79abe16c756b8a0d78
Bhargav Kowshik <bhargav@mapbox.com>
2017-04-21 05:41:50 +0530

    Adding user details to features

--- gabbar/helpers/real_changesets.js
+++ gabbar/helpers/real_changesets.js
@@ -67,6 +67,20 @@ function extract(realChangeset, callback) {
     callback(null, features);
 }
 
+function downloadUserDetails(userID) {
+    return new Promise((resolve, reject) => {
+        try {
+            let url = 'https://osm-comments-api.mapbox.com/api/v1/users/id/' + userID;
+            request.get(url, (error, response, body) => {
+                if (error || response.statusCode !== 200) return resolve({});
+                else return resolve(JSON.parse(body));
+            });
+        } catch (error) {
+            return resolve({});
+        }
+    });
+}
+
 function extractFeatures(realChangeset) {
     return new Promise((resolve, reject) => {
         let changeset = parser(realChangeset);
@@ -75,13 +89,22 @@ function extractFeatures(realChangeset) {
         let featuresModified = getFeaturesByAction(changeset, 'modify');
         let featuresDeleted = getFeaturesByAction(changeset, 'delete');
 
-        let features = {
-            'changeset_id': realChangeset.metadata.id,
-            'features_created': featuresCreated.length,
-            'features_modified': featuresModified.length,
-            'features_deleted': featuresDeleted.length,
-        }
-        resolve(features);
+        let userID = realChangeset.metadata.uid;
+        downloadUserDetails(userID)
+        .then(userDetails => {
+            let features = {
+                'changeset_id': realChangeset.metadata.id,
+                'features_created': featuresCreated.length,
+                'features_modified': featuresModified.length,
+                'features_deleted': featuresDeleted.length,
+                'user_id': userDetails.id,
+                'user_name': userDetails.name,
+                'user_first_edit': userDetails.first_edit,
+                'user_changesets': userDetails.changeset_count,
+                'user_features': userDetails.num_changes
+            };
+            resolve(features);
+        });
     });
 }
 
@@ -90,6 +113,11 @@ function formatFeatures(features) {
         features.changeset_id,
         features.features_created,
         features.features_modified,
-        features.features_deleted
+        features.features_deleted,
+        features.user_id,
+        features.user_name,
+        features.user_first_edit,
+        features.user_changesets,
+        features.user_features
     ];
 }
