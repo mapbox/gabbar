7ecf94b0f66e6116748b840fb46e1d0a2ed80f9e
Bhargav Kowshik <bhargav@mapbox.com>
2017-04-21 05:47:06 +0530

    Remove code referncing older model

--- tests/test_mod.py
+++ tests/test_mod.py
@@ -6,33 +6,6 @@ import os
 
 import gabbar
 
-def test_changeset_to_data():
-    changeset = {
-        "ID": 44581855,
-        "create": 20,
-        "modify": 0,
-        "delete": 0
-    }
-    actual = gabbar.changeset_to_data(changeset)
-    expected = [20, 0, 0]
-    assert actual == expected
-
-def test_predict_problematic():
-    # Modifying 500 features in a changeset is problematic
-    data = [[0, 500, 0]]
-    model = gabbar.load_model()
-    actual = gabbar.predict(model, data)
-    expected = -1  # -1 for outlier.
-    assert actual == expected
-
-def test_predict_not_problematic():
-    # Modifying 5 features in a changeset is not problematic
-    data = [[0, 5, 0]]
-    model = gabbar.load_model()
-    actual = gabbar.predict(model, data)
-    expected = 1  # +1 for inlier.
-    assert actual == expected
-
 def test_get_features():
     changeset_id = u'47734592'
     expected = [changeset_id, 1, 0, 0]
