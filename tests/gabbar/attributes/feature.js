b54e95219d08d6f0973a21483042bea6f74b9d4f
Bhargav Kowshik <bhargav@mapbox.com>
2017-06-23 13:27:31 +0530

    Refactor import, importing feature attributes as one

--- tests/gabbar/attributes/feature.js
+++ tests/gabbar/attributes/feature.js
@@ -5,9 +5,7 @@ var fs = require('fs');
 var path = require('path');
 
 var getSamples = require('../../../gabbar/filters/highway').getSamples;
-var getLineDistance = require('../../../gabbar/attributes/feature').getLineDistance;
-var getKinks = require('../../../gabbar/attributes/feature').getKinks;
-var getAction = require('../../../gabbar/attributes/feature').getAction;
+var fAttributes = require('../../../gabbar/attributes/feature');
 
 test('Get line distance for a highway', function (t) {
     let changesetPath = path.join(__dirname, '../../fixtures/changesets/48255884.json');
@@ -17,8 +15,8 @@ test('Get line distance for a highway', function (t) {
     for (let sample of samples) {
         let newVersion = sample[0];
         let oldVersion = sample[1];
-        t.true(getLineDistance(newVersion) > 0);
-        t.true(getLineDistance(oldVersion) > 0);
+        t.true(fAttributes.getLineDistance(newVersion) > 0);
+        t.true(fAttributes.getLineDistance(oldVersion) > 0);
     }
 
     t.end();
@@ -33,8 +31,8 @@ test('Get kinks for a highway', function (t) {
     for (let sample of samples) {
         let newVersion = sample[0];
         let oldVersion = sample[1];
-        t.equal(getKinks(newVersion).length, 0);
-        t.equal(getKinks(oldVersion).length, 0);
+        t.equal(fAttributes.getKinks(newVersion).length, 0);
+        t.equal(fAttributes.getKinks(oldVersion).length, 0);
     }
 
     t.end();
@@ -48,7 +46,7 @@ test('Get action for a highway', function (t) {
     let samples = getSamples(changeset);
     for (let sample of samples) {
         let newVersion = sample[0];
-        t.equal(getAction(newVersion), 'modify');
+        t.equal(fAttributes.getAction(newVersion), 'modify');
     }
 
     t.end();
