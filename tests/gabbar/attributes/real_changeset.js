afe86359b0e5c6bbe3781a0597dc76cec86afc78
Bhargav Kowshik <bhargav@mapbox.com>
2017-06-30 12:29:13 +0530

    Id is an attribute of a real changeset

--- tests/gabbar/attributes/real_changeset.js
+++ tests/gabbar/attributes/real_changeset.js
@@ -4,12 +4,22 @@ var test = require('tape');
 var fs = require('fs');
 var path = require('path');
 
-var cAttributes = require('../../../gabbar/attributes/real_changeset');
+var rcAttributes = require('../../../gabbar/attributes/real_changeset');
+
 
 test('Get is changeset is labelled harmful on osmcha', function (t) {
     let changesetDir = path.join(__dirname, '../../fixtures/real_changesets/');
     let realChangeset = JSON.parse(fs.readFileSync(path.join(changesetDir, '47734592.json')));
 
-    t.equal(cAttributes.isChangesetHarmful(realChangeset), 0);
+    t.equal(rcAttributes.isChangesetHarmful(realChangeset), 0);
+    t.end();
+});
+
+
+test('Get ID of changeset', function (t) {
+    let changesetDir = path.join(__dirname, '../../fixtures/real_changesets/');
+    let realChangeset = JSON.parse(fs.readFileSync(path.join(changesetDir, '47734592.json')));
+
+    t.equal(rcAttributes.getChangesetID(realChangeset), '47734592');
     t.end();
 });
